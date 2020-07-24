f0b08da32f43d49f6c7c288d55ae2989398399bc
IBJ\IBJ <fortress0427@naver.com>
2020-07-24 22:19:23 +0900

    [feat] Connected react js as bundle.js

--- src/main/java/bjdev/ShowItSelf/ShowItSelfApplication.java
+++ src/main/java/bjdev/ShowItSelf/ShowItSelfApplication.java
@@ -2,10 +2,12 @@ package bjdev.ShowItSelf;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class ShowItSelfApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ShowItSelfApplication.class, args);
 	}
