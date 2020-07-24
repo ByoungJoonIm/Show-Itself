b5689d803b9d02204f873ef8297b794effae03e0
IBJ\IBJ <fortress0427@naver.com>
2020-07-24 14:11:27 +0900

    [feat] Initialized by spring-boot

---
+++ src/main/java/bjdev/ShowItSelf/ShowItSelfApplication.java
@@ -0,0 +1,13 @@
+package bjdev.ShowItSelf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShowItSelfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShowItSelfApplication.class, args);
+	}
+
+}
