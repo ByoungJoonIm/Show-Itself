36a7c18ec6ecb719eefc19a0e06c18130770ec9b
ByoungjoonIm <bj.max.lim@gmail.com>
2020-07-28 20:17:02 +0900

    [refact] Removed unnecessary code.

--- src/main/js/app.js
+++ src/main/js/app.js
@@ -24,7 +24,7 @@ class App extends React.Component{
 				<CssBaseline />
 				<ToolbarWrapper />
 				<HeroUnitWrapper />
-				<Album />
+				<ContentWrapper />
 				{/*
 				<BoxContainerList />
 				<HoverEventExample />
@@ -36,116 +36,6 @@ class App extends React.Component{
 			</div>
 		)
 	}	
-}
-
-class HoverEventExample extends React.Component {
-	constructor(props){
-		super(props);
-		this.handleMouseEnter = this.handleMouseEnter.bind(this);
-		this.handleMouseOver = this.handleMouseOver.bind(this);
-		this.handleMouseLeave = this.handleMouseLeave.bind(this);
-		this.handleMouseOut = this.handleMouseOut.bind(this);
-	}
-	
-	handleMouseEnter(e){
-		e.target.style.background = 'red';
-	}
-	
-	handleMouseOver(e){
-		e.target.style.background = 'blue';
-	}
-	
-	handleMouseLeave(e){
-		e.target.style.background = 'yellow';
-	}
-	
-	handleMouseOut(e){
-		e.target.style.background = 'black';
-	}
-	
-	render(){
-		return(
-			<div>
-				<button 
-					onMouseEnter={this.handleMouseEnter}
-					onMouseLeave={this.handleMouseLeave}
-				>hover here!</button>
-				<button 
-					onMouseOver={this.handleMouseOver}
-					onMouseOut={this.handleMouseOut}
-				>hover here!</button>
-			</div>
-		)
-	}
-}
-
-class BoxContainer extends React.Component{
-	constructor(props){
-		super(props);
-		this.handleMouseOver = this.handleMouseOver.bind(this);
-		this.handleMouseOut = this.handleMouseOut.bind(this);
-		
-		this.state = {hoverText: "Not hovered",
-						isHover: false};
-	}
-	
-	handleMouseOver(e){
-		e.preventDefault(e);
-		this.setState({isHover: true});
-		this.setState({hoverText: "Hovered"});
-	}
-	
-	handleMouseOut(e){
-		e.preventDefault(e);
-		this.setState({isHover: false});
-		this.setState({hoverText: "Not hovered"});
-	}
-	
-	render(){
-		const hoverContent = (this.state.isHover) ? 
-			<div>
-					<p>detail description</p>
-					<a>->see detail</a>
-			</div>
-			:
-			<div>			{/* toggle simple desc and detail desc */}
-				<div><p>image icon here</p></div>	{/* image container */}		
-				<h3>title</h3>	{/* titme container */}
-				<p>description</p>	{/* description container */}
-					
-			</div>
-			;
-			
-		const tempStyle={
-			width: "200px",
-			height: "200px",
-			background: "blue",
-		}
-					
-		return(
-			<div>
-				<div
-					style={tempStyle}			
-					onMouseOver={this.handleMouseOver}
-					onMouseOut={this.handleMouseOut}
-				>				{/* hover container */}
-					{hoverContent}
-				</div>
-				
-				<p>{this.state.hoverText}</p>
-			</div>
-		)
-	}
-}
-
-class BoxContainerList extends React.Component{
-	render(){
-		return(
-			<div>
-				<BoxContainer />
-			</div>
-		)
-	}
 }
 
 class ToolbarWrapper extends React.Component{
@@ -171,6 +61,30 @@ class ToolbarWrapper extends React.Component{
 			</AppBar>
 		)
 	}	
+}
+
+class HeroUnitWrapper extends React.Component{
+	render(){	
+		const styles = makeStyles((theme) => ({
+			heroContent: {
+				backgroundColor: theme.palette.background.paper,
+    			padding: theme.spacing(8, 0, 6),
+  			},
+		}));
+		
+		return (
+			<div className={styles.heroContent}>
+				<Container maxWidth="sm">
+					<Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom>
+					Show itself
+					</Typography>
+					<Typography variant="h5" align="center" color="textSecondary" paragraph>
+					This site introduces itself. I'm trying to use current technologies for web over full stack. It describes how it works and what technoloes used.
+					</Typography>
+				</Container>
+			</div>
+		);
+	}
 }
 
 class CardWrapper extends React.Component{
@@ -217,7 +131,7 @@ class CardWrapper extends React.Component{
 			</Card>
 			
 			:
-			<div style = {divstyle}>
+			<div style={divstyle}>
 				<div style = {{
 					background: 'blue',
 					height: '200px',
@@ -260,7 +174,7 @@ class CardWrapper extends React.Component{
 	}
 }
 
-class Album extends React.Component{
+class ContentWrapper extends React.Component{
 	render(){	
 		const styles = makeStyles((theme) => ({
 			cardGrid: {
@@ -294,30 +208,6 @@ class Album extends React.Component{
 
 }
 
-class HeroUnitWrapper extends React.Component{
-	render(){	
-		const styles = makeStyles((theme) => ({
-			heroContent: {
-				backgroundColor: theme.palette.background.paper,
-    			padding: theme.spacing(8, 0, 6),
-  			},
-		}));
-		
-		return (
-			<div className={styles.heroContent}>
-				<Container maxWidth="sm">
-					<Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom>
-					Show itself
-					</Typography>
-					<Typography variant="h5" align="center" color="textSecondary" paragraph>
-					This site introduces itself. I'm trying to use current technologies for web over full stack. It describes how it works and what technoloes used.
-					</Typography>
-				</Container>
-			</div>
-		);
-	}
-}
-
 class FooterWrapper extends React.Component{
 	render(){
 		const style = makeStyles((theme) => ({
@@ -346,7 +236,6 @@ class FooterWrapper extends React.Component{
 		);
 	}
 }
-
 
 ReactDOM.render(
 	<App />,
