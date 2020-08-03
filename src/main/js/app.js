73d64801653bece5d6ae45b5819f7ab0e6d6820d
ByoungjoonIm <bj.max.lim@gmail.com>
2020-08-03 15:21:30 +0900

    [refact] styles variable moved upper and merged with others.

--- src/main/js/app.js
+++ src/main/js/app.js
@@ -18,20 +18,35 @@ import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import Link from '@material-ui/core/Link';
 
-class App extends React.Component{
-	constructor(props){
+const styles = makeStyles((theme) => ({
+	heroContent: {
+		backgroundColor: theme.palette.background.paper,
+		padding: theme.spacing(8, 0, 6),
+	},
+	footer: {
+		backgroundColor: theme.palette.background.paper,
+		padding: theme.spacing(6),
+	},
+	cardGrid: {
+		paddingTop: theme.spacing(8),
+		paddingBottom: theme.spacing(8),
+	},
+}));
+
+class App extends React.Component {
+	constructor(props) {
 		super(props);
-		this.state = {cardContents: []};
+		this.state = { cardContents: [] };
 	}
-	
-	componentDidMount(){
-		client({method: 'GET', path: '/api/cardContents'}).done(response => {
-			this.setState({cardContents: response.entity._embedded.cardContents});
+
+	componentDidMount() {
+		client({ method: 'GET', path: '/api/cardContents' }).done(response => {
+			this.setState({ cardContents: response.entity._embedded.cardContents });
 		});
 	}
-	
-	render(){
-		return(
+
+	render() {
+		return (
 			<div>
 				<CssBaseline />
 				<ToolbarWrapper />
@@ -40,18 +55,18 @@ class App extends React.Component{
 				<FooterWrapper />
 			</div>
 		)
-	}	
+	}
 }
 
-class ToolbarWrapper extends React.Component{
-	render(){
-		return(
+class ToolbarWrapper extends React.Component {
+	render() {
+		return (
 			<AppBar position="static">
 				<Toolbar>
 					<Grid container alignItems='center'>
 						<Grid item>
 							<Button color="inherit">
-							<Typography variant="h6" color="inherit" noWrap>bjdev</Typography>
+								<Typography variant="h6" color="inherit" noWrap>bjdev</Typography>
 							</Button>
 						</Grid>
 						<Grid item xs />
@@ -65,26 +80,19 @@ class ToolbarWrapper extends React.Component{
 				</Toolbar>
 			</AppBar>
 		)
-	}	
+	}
 }
 
-class HeroUnitWrapper extends React.Component{
-	render(){	
-		const styles = makeStyles((theme) => ({
-			heroContent: {
-				backgroundColor: theme.palette.background.paper,
-    			padding: theme.spacing(8, 0, 6),
-  			},
-		}));
-		
+class HeroUnitWrapper extends React.Component {
+	render() {
 		return (
 			<div className={styles.heroContent}>
 				<Container maxWidth="sm">
 					<Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom>
-					Show itself
+						Show itself
 					</Typography>
 					<Typography variant="h5" align="center" color="textSecondary" paragraph>
-					This site introduces itself. I'm trying to use current technologies for web over full stack. It describes how it works and what technoloes used.
+						This site introduces itself. I'm trying to use current technologies for web over full stack. It describes how it works and what technoloes used.
 					</Typography>
 				</Container>
 			</div>
@@ -92,77 +100,77 @@ class HeroUnitWrapper extends React.Component{
 	}
 }
 
-class CardWrapper extends React.Component{
-	constructor(props){
+class CardWrapper extends React.Component {
+	constructor(props) {
 		super(props);
 		this.handleMouseEnter = this.handleMouseEnter.bind(this);
 		this.handleMouseLeave = this.handleMouseLeave.bind(this);
-		
-		this.state = {isHover : false};
+
+		this.state = { isHover: false };
 	}
-	
-	handleMouseEnter(e){
+
+	handleMouseEnter(e) {
 		e.preventDefault(e);
-		this.setState({isHover : true});
+		this.setState({ isHover: true });
 	}
-	
-	handleMouseLeave(e){
+
+	handleMouseLeave(e) {
 		e.preventDefault(e);
-		this.setState({isHover : false});
+		this.setState({ isHover: false });
 	}
-	
-	render(){
+
+	render() {
 		const frameStyle = {
-					width: '270px',
-					height: '270px',
-					border: '1px solid black'
-				};
-		
+			width: '270px',
+			height: '270px',
+			border: '1px solid black'
+		};
+
 		const hoveredContent =
-			<div style = {frameStyle}>
-				<p style ={{
+			<div style={frameStyle}>
+				<p style={{
 					wordWrap: 'break-word',
 					textAlign: 'center'
 				}}>{this.props.cardContent.description}
-				</p>	
+				</p>
 			</div>
-		const nonHoveredContent = 
+		const nonHoveredContent =
 			<div style={frameStyle}>
-				<div style = {{
+				<div style={{
 					background: 'blue',
 					height: '200px',
 					width: '270px',
 					padding: '5px 5px 5px 5px'
 				}}>
-					<div style ={{
+					<div style={{
 						background: 'green',
 						height: '190px',
 						width: '260px',
 					}}>
 						<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png" style={{
 							height: '190px',
-							width: '260px'	
-						}}/>
+							width: '260px'
+						}} />
 					</div>
 				</div>
-				<div style = {{
+				<div style={{
 					background: 'pink',
 					height: '70px',
 					width: '270px',
 					position: 'absolute'
 				}}>
-					<h1 style ={{
+					<h1 style={{
 						textAlign: 'center',
 						verticalAlign: 'middle'
 					}}>{this.props.cardContent.name}</h1>
 				</div>
 			</div>
-		
-		return(
+
+		return (
 			<div
-				onMouseEnter = {this.handleMouseEnter}
-				onMouseLeave = {this.handleMouseLeave}
-				style = {{
+				onMouseEnter={this.handleMouseEnter}
+				onMouseLeave={this.handleMouseLeave}
+				style={{
 					width: '270px',
 					height: '270px',
 					background: 'orange'
@@ -174,49 +182,35 @@ class CardWrapper extends React.Component{
 	}
 }
 
-class ContentWrapper extends React.Component{
-	render(){	
-		const styles = makeStyles((theme) => ({
-			cardGrid: {
-				paddingTop: theme.spacing(8),
-				paddingBottom: theme.spacing(8),
-			},
-		}));
-
+class ContentWrapper extends React.Component {
+	render() {
 		const cardContents = this.props.cardContents.map(cardContent =>
 			<Grid item key={cardContent._links.self.href} xs={12} sm={6} md={4} lg={3} zeroMinWidth>
-				<CardWrapper cardContent={cardContent}/>
+				<CardWrapper cardContent={cardContent} />
 			</Grid>
 		);
-		
-		return(
-		
-        <Container className={styles.cardGrid} maxWidth="lg">
-          {/* End hero unit */}
-          <Grid container spacing={4}>
-				{cardContents}				
-          </Grid>
-        </Container>
+
+		return (
+
+			<Container className={styles.cardGrid} maxWidth="lg">
+				{/* End hero unit */}
+				<Grid container spacing={4}>
+					{cardContents}
+				</Grid>
+			</Container>
 		)
 	}
-
 }
 
-class FooterWrapper extends React.Component{
-	render(){
-		const style = makeStyles((theme) => ({
-			footer: {
-			backgroundColor: theme.palette.background.paper,
-			padding: theme.spacing(6),
-		}}));
-		
-		return(
-			<footer className={style.footer}>
-        		<Typography variant="h6" align="center" gutterBottom>Footer</Typography>
+class FooterWrapper extends React.Component {
+	render() {
+		return (
+			<footer className={styles.footer}>
+				<Typography variant="h6" align="center" gutterBottom>Footer</Typography>
 				<Typography variant="subtitle1" align="center" color="textSecondary" component="p">
-				Something here to give the footer a purpose!
+					Something here to give the footer a purpose!
 				</Typography>
-				
+
 				{/* copyright */}
 				<Typography variant="body2" color="textSecondary" align="center">
 					{'Copyright © '}
@@ -225,7 +219,7 @@ class FooterWrapper extends React.Component{
 					</Link>{' '}
 					{new Date().getFullYear()}
 					{'.'}
-				</Typography>				
+				</Typography>
 			</footer>
 		);
 	}
