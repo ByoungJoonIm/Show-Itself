6d13ef5e08c94d9942e36c912eef32456908ac2d
ByoungjoonIm <bj.max.lim@gmail.com>
2020-08-07 21:50:28 +0900

    [feat] Added TechnologyCardProcess class. It adds additional link while basic json format is remaining.

---
+++ src/main/java/bjdev/ShowItSelf/TechnologyCardProcessor.java
@@ -0,0 +1,27 @@
+package bjdev.ShowItSelf;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+import java.io.IOException;
+
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.RepresentationModelProcessor;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TechnologyCardProcessor implements RepresentationModelProcessor<EntityModel<TechnologyCard>> {
+    @Override
+    public EntityModel<TechnologyCard> process(EntityModel<TechnologyCard> resource) {
+
+    	TechnologyCard technologyCard = resource.getContent();
+    	
+    	try {
+        	resource.add(linkTo(methodOn(IconImageController.class).getAnImageWithMediaType(technologyCard.getIconName())).withRel("iconImageLink"));    		
+    	} catch(IOException ioe) {
+    		ioe.printStackTrace();;
+    	}
+    	
+        return resource;
+    }
+}
