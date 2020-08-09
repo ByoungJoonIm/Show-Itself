17df781e9c75c1d19a735f32b105ca1769f058b4
ByoungjoonIm <bj.max.lim@gmail.com>
2020-08-09 13:54:22 +0900

    [feat] IconName field was added to TechniqueCard class too.

--- src/main/java/bjdev/ShowItSelf/TechniqueCard.java
+++ src/main/java/bjdev/ShowItSelf/TechniqueCard.java
@@ -6,17 +6,25 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 @Entity
 public class TechniqueCard{
 	private @Id @GeneratedValue Long id;
 	private String name;
 	private String description;
+	private @JsonIgnore String iconName;
 	
 	private TechniqueCard() {}
 	
 	public TechniqueCard(String name, String description) {
+		this(name, description, "not-found");
+	}
+	
+	public TechniqueCard(String name, String description, String iconName) {
 		this.name = name;
 		this.description = description;
+		this.iconName = iconName;
 	}
 	
 	@Override
@@ -28,12 +36,13 @@ public class TechniqueCard{
 		TechniqueCard techniqueCard = (TechniqueCard) o;
 		return Objects.equals(id, techniqueCard.id) &&
 				Objects.equals(name, techniqueCard.name) &&
-				Objects.equals(description, techniqueCard.description);
+				Objects.equals(description, techniqueCard.description) &&
+				Objects.equals(iconName, techniqueCard.iconName);
 	}
 	
 	@Override
 	public int hashCode() {
-		return Objects.hash(id, name, description);
+		return Objects.hash(id, name, description, iconName);
 	}
 
 	public Long getId() {
@@ -59,9 +68,19 @@ public class TechniqueCard{
 	public void setDescription(String description) {
 		this.description = description;
 	}
+	
+	
+	public String getIconName() {
+		return iconName;
+	}
+
+	public void setIconName(String iconName) {
+		this.iconName = iconName;
+	}
 
 	@Override
 	public String toString() {
-		return "TechniqueCard [id=" + id + ", name=" + name + ", description=" + description + "]";
+		return "TechniqueCard [id=" + id + ", name=" + name + ", description=" + description + ", iconName=" + iconName
+				+ "]";
 	}
 }
