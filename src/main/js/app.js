b8aefe31030a9b935b1b0aea7aab7f2a28ab5e83
ByoungjoonIm <bj.max.lim@gmail.com>
2020-08-09 13:41:41 +0900

    [feat] Removed unnecessary import and comment.

--- src/main/js/app.js
+++ src/main/js/app.js
@@ -6,10 +6,6 @@ const client = require('./client');
 
 import AppBar from '@material-ui/core/AppBar';
 import Button from '@material-ui/core/Button';
-import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Grid from '@material-ui/core/Grid';
 import Toolbar from '@material-ui/core/Toolbar';
@@ -165,7 +161,6 @@ class CardWrapper extends React.Component {
 						height: '190px',
 						width: '260px',
 					}}>
-					{/* follow url will be changed to rest api uri*/}
 						<img src={imageURI} style={{
 							height: '190px',
 							width: '260px'
