89af7746af182d87246992db6ef430ea5e2054c3
byoungjoonim <bj.max.lim@gmail.com>
2020-08-14 23:48:13 +0900

    [feat] Added something in Dockerfile and entrypoint.sh

--- Dockerfile
+++ Dockerfile
@@ -3,5 +3,6 @@ WORKDIR /root
 COPY entrypoint.sh run.sh
 RUN chmod 700 run.sh
 ENV DEBIAN_FRONTEND=noninteractive
+EXPOSE 80/tcp
 
 ENTRYPOINT ["./run.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -2,3 +2,9 @@
 
 apt update
 apt install git openjdk-8-jdk nodejs npm -y
+git clone https://github.com/ByoungJoonIm/Show-Itself
+cd Show-Itself
+chmod 700 gradlew
+./gradlew webpack
+./gradlew build
+#java -jar build/libs/ShowItSelf-0.0.1-SNAPSHOT.jar
