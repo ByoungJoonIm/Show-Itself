194ab7fa68d597eb720a1528fd718ebbf1d904d8
ByoungjoonIm <bj.max.lim@gmail.com>
2020-08-29 10:03:01 +0900

    [feat] Controller replaced to config.

---
+++ src/main/java/bjdev/ShowItSelf/MvcConfig.java
@@ -0,0 +1,18 @@
+package bjdev.ShowItSelf;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MvcConfig implements WebMvcConfigurer{
+	private static final String HOME = "index";
+	private static final String DOCS = "docs";
+	
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/home").setViewName(HOME);
+		registry.addViewController("/").setViewName(HOME);
+		registry.addViewController("/docs").setViewName(DOCS);
+	}
+}
