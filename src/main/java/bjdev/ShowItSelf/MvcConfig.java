ea09d936c608e4f4c83707a7486f61c752fb40ef
ByoungjoonIm <bj.max.lim@gmail.com>
2020-09-02 18:10:54 +0900

    [feat] Indicator was changed.

--- src/main/java/bjdev/ShowItSelf/MvcConfig.java
+++ src/main/java/bjdev/ShowItSelf/MvcConfig.java
@@ -6,8 +6,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class MvcConfig implements WebMvcConfigurer{
-	private static final String HOME = "index";
-	private static final String DOCS = "docs";
+	static final String HOME = "index";
+	static final String DOCS = "docs";
 	
 	@Override
 	public void addViewControllers(ViewControllerRegistry registry) {
